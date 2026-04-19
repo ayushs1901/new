b87374cdf72b6f702217d67abf9dc6f3e044de51
ayushs1901 <ayush.s1901@gmail.com>
2026-04-20 02:53:23 +0530

    Commit

---
+++ chatbot.py
@@ -0,0 +1,17 @@
+def chatbot():
+    """Simple chatbot that replies with 'hi'"""
+    print("Chatbot: Hi! Type something or 'quit' to exit")
+    
+    while True:
+        user_input = input("You: ").strip()
+        
+        if user_input.lower() in ['quit', 'exit', 'bye']:
+            print("Chatbot: Goodbye!")
+            break
+        elif user_input:
+            print("Chatbot: Hi")
+        else:
+            print("Chatbot: Hi")
+
+if __name__ == "__main__":
+    chatbot()
